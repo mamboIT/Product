f0e93f855c3cb9011a5e58f7d0eb73244848be37
Micaela Cavallo <36814776+swifterH@users.noreply.github.com>
2019-11-20 14:24:48 +0100

    updated with challenges detail controller

---
+++ fireworks/fireworks/ChallengeDetailsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ChallengeDetailsViewController.swift
+//  fireworks
+//
+//  Created by Micaela Cavallo on 20/11/2019.
+//  Copyright © 2019 Terrence Gillespie. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeDetailsViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var button: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //further modify style (font) etc. 
+    
+    }
+    
+  
+}
